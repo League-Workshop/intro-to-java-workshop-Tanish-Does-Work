9f71a07ee6802c6c7a9a4556847aaa41a8afc244
root <root@3c0e175b7c6c>
2020-03-31 21:56:27 +0000

    Day 2

--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -6,10 +6,29 @@ import org.jointheleague.graphical.robot.Robot;
 public class MyFirstJavaProgram {
 	
 	public static void main(String[] args) {
-		
-		// START HERE
+		Robot bob = new Robot("batman");
+for( int i = 0; i < 4; i++){
+bob.setRandomPenColor();
+		}
+bob.setSpeed(0);
+bob.setPenWidth(100);
+bob.hide();
+for( int i = 0; i < 1000; i++){
 	
+	bob.setRandomPenColor();
+	bob.setSpeed(1000000);
+	bob.penDown();
+	//bob.setAngle(i);
+	//bob.move(i);
+	bob.turn(90);
+	bob.move(i);
+	int x = bob.getX();
+	int y = bob.getY();
+	if(x >= 1000) {
+		bob.setAngle(-i);
+	}
+}
 		
-		
+			
 	}
 }
